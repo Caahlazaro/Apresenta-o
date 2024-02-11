b660467e7509641a0f0119682348bcad33f0cd61
Caahlazaro <carolinelazarodasilva@gmail.com>
2024-02-10 22:50:00 -0300

    Apresentação simples

---
+++ apresentação.js
@@ -0,0 +1,18 @@
+// Definindo variáveis para informações pessoais
+const nome = "Caroline";
+const idade = 33;
+const autista = "Autista nível 2 de Suporte";
+const faculdade = "Gestão da Tecnologia da Informação";
+const hiperfoco = "Tecnologia e Programação";
+const filhos = "Jeane de 10 anos e Miguel de 7 anos";
+const emprego = "Transição de Carreira para Área Tech";
+
+// Imprimindo informações no console
+console.log(`Olá, meu nome é ${nome}`);  // Apresentando o nome
+console.log(`Tenho ${idade} anos de idade.`);  // Apresentando a idade
+console.log(`Sou ${autista}.`);  // Apresentando o status de autista
+console.log(`Estou na faculdade de ${faculdade}.`);  // Apresentando a faculdade
+console.log(`Meu hiperfoco é ${hiperfoco}.`);  // Apresentando o hiperfoco
+console.log(`Tenho 2 filhos: ${filhos}.`);  // Apresentando informações sobre os filhos
+console.log(`Estou em ${emprego}.`);  // Apresentando o emprego atual
+
